64ecf8859e3c6aeebcf0cb61242201291781c27c
Fahrel <fahrel@students.undip.ac.id>
2023-11-15 16:20:08 +0700

    feat: improve styling

--- components/calculator.tsx
+++ components/calculator.tsx
@@ -60,40 +60,43 @@ const Calculator: React.FC = () => {
   };
 
   return (
-    <div className="max-w-xs mx-auto py-10">
-      <div
-        id="calculation"
-        className="text-xs text-gray-400 p-4 text-right font-bold rounded mb-1"
-      >
-        {result !== null && result}
-        {calculation !== null && calculation}
-        {!["+", "-", "x", "/"].includes(currentDisplay) && currentDisplay}
-      </div>
+    <div className="max-w-xs mx-auto bg-gray-200 p-4 rounded-lg shadow-md m-12">
+      <p className="text-xs text-center my-2">Calculator by dafex301</p>
       <div
         id="currentDisplay"
-        className="bg-black text-white p-4 text-right text-2xl font-bold rounded mb-4"
+        className="bg-black relative text-white p-4 pt-8 text-right text-2xl font-bold rounded mb-4"
       >
+        <div
+          id="calculation"
+          className="text-xs text-gray-400 text-right font-bold rounded mb-1 absolute top-3 right-4"
+        >
+          {result !== null && result}
+          {calculation !== null && calculation}
+          {!["+", "-", "x", "/"].includes(currentDisplay) &&
+            currentDisplay !== "0" &&
+            currentDisplay}
+        </div>
         {currentDisplay}
       </div>
       <div className="grid grid-cols-4 gap-2">
         <button
           onClick={() => handleButtonClick("C")}
           id="clear"
-          className="col-span-2 bg-red-600 text-white font-bold py-2 rounded"
+          className="col-span-2 bg-red-600 text-white font-bold py-2 rounded hover:bg-red-800 transition-all"
         >
           AC
         </button>
         <button
           onClick={() => handleButtonClick("/")}
           id="divide"
-          className="bg-gray-600 text-white font-bold py-2 rounded"
+          className="bg-gray-600 text-white font-bold py-2 rounded hover:bg-gray-800 transition-all"
         >
           /
         </button>
         <button
           onClick={() => handleButtonClick("x")}
           id="multiply"
-          className="bg-gray-600 text-white font-bold py-2 rounded"
+          className="bg-gray-600 text-white font-bold py-2 rounded hover:bg-gray-800 transition-all"
         >
           x
         </button>
@@ -103,7 +106,11 @@ const Calculator: React.FC = () => {
               key={number}
               onClick={() => handleButtonClick(number)}
               id={number}
-              className="bg-gray-700 text-white font-bold py-2 rounded"
+              className={
+                !["+", "-"].includes(number)
+                  ? "bg-gray-700 text-white font-bold py-2 rounded hover:bg-gray-800 transition-all"
+                  : "bg-gray-600 text-white font-bold py-2 rounded hover:bg-gray-700 transition-all"
+              }
             >
               {number}
             </button>
@@ -112,21 +119,21 @@ const Calculator: React.FC = () => {
         <button
           onClick={() => handleButtonClick("=")}
           id="equals"
-          className="col-span-1 row-span-2 bg-blue-700 text-white font-bold py-2 rounded"
+          className="col-span-1 row-span-2 bg-blue-700 hover:bg-blue-900 transition-all text-white font-bold py-2 rounded"
         >
           =
         </button>
         <button
           onClick={() => handleButtonClick("0")}
           id="zero"
-          className="col-span-2 bg-gray-700 text-white font-bold py-2 rounded"
+          className="col-span-2 bg-gray-700 text-white font-bold py-2 rounded hover:bg-gray-800 transition-all"
         >
           0
         </button>
         <button
           onClick={() => handleButtonClick(".")}
           id="decimal"
-          className="bg-gray-700 text-white font-bold py-2 rounded"
+          className="bg-gray-700 text-white font-bold py-2 rounded hover:bg-gray-800 transition-all"
         >
           .
         </button>
